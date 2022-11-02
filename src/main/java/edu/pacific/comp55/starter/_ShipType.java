7c1f634504d061a9485c031ddbc66a1199f679ec
NathanOsorio <n_osorio2@u.pacific.edu>
2022-11-02 10:28:58 -0700

    updated shiptype enums

--- src/main/java/edu/pacific/comp55/starter/_ShipType.java
+++ src/main/java/edu/pacific/comp55/starter/_ShipType.java
@@ -1,12 +1,12 @@
 package edu.pacific.comp55.starter;
 public enum _ShipType {
-	MYCAR, TRUCK, AUTO;
+	SHIP_BALANCED, SHIP_TANK, SHIP_SPEED;
 //yo	
 	public String toString() {
 		switch(this) {
-			case MYCAR: return "car";
-			case TRUCK: return "truck";
-			case AUTO: return "auto";
+			case SHIP_BALANCED: return "balanced";
+			case SHIP_TANK: return "tank";
+			case SHIP_SPEED: return "speed";
 		}
 		return "n/a";
 	}
