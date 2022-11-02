841c24d3f782fac0ac56403d43a4e65e36b9618c
NathanOsorio <n_osorio2@u.pacific.edu>
2022-11-02 10:49:53 -0700

    built constructor to use for our ships along with establishing its getters and setters :)

--- src/main/java/edu/pacific/comp55/starter/_Ships.java
+++ src/main/java/edu/pacific/comp55/starter/_Ships.java
@@ -1,5 +1,44 @@
 package edu.pacific.comp55.starter;
 
 public class _Ships {
+	
+	
+	int damage; 
+	int health; 
+	boolean isSpeed; 
+	// ask professor how to go about implementing speed. Should it be a boolean or an  int?
+	// constructor to use for ships 
+	public _Ships(_ShipType type, int health, int damage, boolean isSpeed) {
+		
+		this.type = type; 
+		this.damage = damage; 
+		this.isSpeed = isSpeed; 
+	}
+	
+	_ShipType type; 
+	public _ShipType getType() {
+		return type;
+	}
+	public void setType(_ShipType type) {
+		this.type = type;
+	}
+	public int getDamage() {
+		return damage;
+	}
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public boolean isSpeed() {
+		return isSpeed;
+	}
+	public void setSpeed(boolean isSpeed) {
+		this.isSpeed = isSpeed;
+	}
 
 }
