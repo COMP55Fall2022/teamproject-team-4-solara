4d46ac827ebdac6aa9456c74ab95203c2c521b6a
PropositionalLogic <m_constantinoguzman@u.pacific.edu>
2022-11-03 16:16:18 -0700

    Added SimplePoint class for Spawn points

---
+++ src/main/java/edu/pacific/comp55/starter/SimplePoint.java
@@ -0,0 +1,25 @@
+package edu.pacific.comp55.starter;
+
+public class SimplePoint {
+	  private double x, y;
+
+	  public SimplePoint(double x, double y) {
+	    x = x;
+	    y = y;
+	  }
+
+	  public double getX() { return x; };
+
+	  public double getY() { return y; };
+
+	  public double distance(SimplePoint p) {
+	    return Math.sqrt( Math.pow(p.getX()-x,2) + Math.pow(p.getY()-y,2) );
+	  }
+
+	  public static void main(String[] args) {
+	    SimplePoint p1 = new SimplePoint(2,1);
+	    SimplePoint p2 = new SimplePoint(8,2);
+
+	    System.out.println( p1.distance(p2) ); // 6.082762530298219
+	  }
+}
