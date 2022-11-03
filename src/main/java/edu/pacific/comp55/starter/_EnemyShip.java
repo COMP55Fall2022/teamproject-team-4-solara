828833b83f879c2bcee40e17e3924a2c4e4b4a18
Divine <d_atah@u.pacific.edu>
2022-11-03 13:45:14 -0700

    Filled out some methods and added increment and decrement functions.
    I also added an array to keep the _EnemyShip objects, added a ToString,
    and imported java.util.*.

--- src/main/java/edu/pacific/comp55/starter/_EnemyShip.java
+++ src/main/java/edu/pacific/comp55/starter/_EnemyShip.java
@@ -1,4 +1,5 @@
 package edu.pacific.comp55.starter;
+import java.util.*;
 
 public class _EnemyShip extends _Ships{
 
@@ -6,8 +7,42 @@ public class _EnemyShip extends _Ships{
 		super(type, health, damage, isSpeed);
 		// TODO Auto-generated constructor stub
 	}
-	private void addEnemyShip() {}
-	private void removeEnemyShip() {}
-	private void bulletsShooting() {}
+	
+	private int enemyShipNum;
+	private Timer movementTimer;
+	private ArrayList<_Ships> shipList= new ArrayList<_Ships>();;
+	
+	private void incrementShips() {
+		enemyShipNum++;
+	}
+	private void decrementShips() {
+		enemyShipNum--;
+	}
+
+	/*
+	 * private int getShipNum() { return enemyShipNum; }
+	 */
+	private void addEnemyShip(_Ships s) {
+		s = new _Ships(s.type, s.health, s.damage, s.isSpeed);
+		shipList.add(s);
+		incrementShips();
+	}
+	private void removeEnemyShip() {
 		
+		decrementShips();
+	}
+	private void bulletsShooting() {
+		
+	}
+	
+	
+	@Override
+	public String toString() {
+		return ("Vehicle info" + "\n -----------" + "\nType of ship: " + this.type + "\nhealth of vehicle: " + this.health + "\ndamage of vehicle: " + this.damage
+				+ "\nspeed level: " + this.isSpeed  );
+	}
+	public static void main() {
+		_Ships grunt1 = new _Ships(_ShipType.GRUNT, 3, 3, 3 );
+		//addEnemyShip(s);
+	}
 }
