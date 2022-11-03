99444967100cf084deca4f7015c26a0deccb987e
Miguel Constantino-Guzman <m_constantinoguzman@u.pacific.edu>
2022-11-02 21:45:58 -0700

    No change - Miguel

--- src/main/java/edu/pacific/comp55/starter/_BattleShip.java
+++ src/main/java/edu/pacific/comp55/starter/_BattleShip.java
@@ -1,11 +1,22 @@
 package edu.pacific.comp55.starter;
 
+// PLAYER ship class inherited from Ship class
 public class _BattleShip extends _Ships {
+	
+	
 	
 	public _BattleShip(_ShipType type, int health, int damage, int isSpeed) {
 		super(type, health, damage, isSpeed);
 		// TODO Auto-generated constructor stub
 	}
+	
+	// Testing things out - Miguel
+	@Override
+	public String toString() {
+		return super.toString() + "Hello";
+	}
+	
+	
 	private void addPlayers() {}
 	private void removePlayers() {}
 	private void moveShip() {}
