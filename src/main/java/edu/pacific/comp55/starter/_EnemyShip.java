dbd697294c30f6a45c69190190744d984b896875
Divine <d_atah@u.pacific.edu>
2022-11-03 14:13:33 -0700

    Changed formatting of the main in _Ships to make it more readable, and I filled out the main in the _EnemyShip class. It doesn't run but it gives the right output.

--- src/main/java/edu/pacific/comp55/starter/_EnemyShip.java
+++ src/main/java/edu/pacific/comp55/starter/_EnemyShip.java
@@ -1,48 +1,54 @@
 package edu.pacific.comp55.starter;
+
 import java.util.*;
 
-public class _EnemyShip extends _Ships{
+public class _EnemyShip extends _Ships {
 
 	public _EnemyShip(_ShipType type, int health, int damage, int isSpeed) {
 		super(type, health, damage, isSpeed);
 		// TODO Auto-generated constructor stub
 	}
-	
-	private int enemyShipNum;
+
+	private int enemyShipNum = 0;
 	private Timer movementTimer;
-	private ArrayList<_Ships> shipList= new ArrayList<_Ships>();;
-	
+	private ArrayList<_Ships> shipList = new ArrayList<_Ships>();;
+
 	private void incrementShips() {
 		enemyShipNum++;
 	}
+
 	private void decrementShips() {
 		enemyShipNum--;
 	}
 
-	/*
-	 * private int getShipNum() { return enemyShipNum; }
-	 */
-	private void addEnemyShip(_Ships s) {
+
+	public int getShipNum() { 
+		return enemyShipNum;
+	}
+
+	public void addEnemyShip(_Ships s) { s = new _Ships(s.type, s.health,
+			s.damage, s.isSpeed); shipList.add(s); incrementShips();
+	}
+	private void removeEnemyShip(_Ships s) {
 		s = new _Ships(s.type, s.health, s.damage, s.isSpeed);
-		shipList.add(s);
-		incrementShips();
-	}
-	private void removeEnemyShip() {
-		
-		decrementShips();
-	}
-	private void bulletsShooting() {
-		
-	}
-	
-	
+		decrementShips(); 
+	} 
+	private void bulletsShooting() {}
+
+
 	@Override
-	public String toString() {
+	public String toString() { 
 		return ("Vehicle info" + "\n -----------" + "\nType of ship: " + this.type + "\nhealth of vehicle: " + this.health + "\ndamage of vehicle: " + this.damage
 				+ "\nspeed level: " + this.isSpeed  );
 	}
+	
+
 	public static void main() {
-		_Ships grunt1 = new _Ships(_ShipType.GRUNT, 3, 3, 3 );
-		//addEnemyShip(s);
+		_EnemyShip grunt1 = new _EnemyShip(_ShipType.GRUNT, 3, 3, 3);
+		grunt1.addEnemyShip(grunt1);
+		System.out.println("There is 1 grunt with:");
+		System.out.println(grunt1);
+		System.out.println("Number of enemy ships in the array: " + grunt1.getShipNum());
+		
 	}
 }
--- src/main/java/edu/pacific/comp55/starter/_Ships.java
+++ src/main/java/edu/pacific/comp55/starter/_Ships.java
@@ -1,70 +1,73 @@
 package edu.pacific.comp55.starter;
+
 import acm.graphics.*;
+
 public class _Ships {
 
-	protected int healthScore; 
-	protected int damage; 
-	protected int health; 
-	protected int isSpeed; 
-	
+	protected int healthScore;
+	protected int damage;
+	protected int health;
+	protected int isSpeed;
+
 	public _Ships(_ShipType type, int health, int damage, int isSpeed) {
-		this.health = health; 
-		this.type = type; 
-		this.damage = damage; 
-		this.isSpeed = isSpeed; 
+		this.health = health;
+		this.type = type;
+		this.damage = damage;
+		this.isSpeed = isSpeed;
 	}
-	
-	
-	_ShipType type; 
+
+	_ShipType type;
+
 	public _ShipType getType() {
 		return type;
 	}
+
 	public void setType(_ShipType type) {
 		this.type = type;
 	}
+
 	public int getDamage() {
 		return damage;
 	}
+
 	public void setDamage(int damage) {
 		this.damage = damage;
 	}
+
 	public int getHealth() {
 		return health;
 	}
+
 	public void setHealth(int health) {
 		this.health = health;
 	}
+
 	public int getSpeed() {
 		return isSpeed;
 	}
+
 	public void setSpeed(int isSpeed) {
 		this.isSpeed = isSpeed;
 	}
 
-	public String toString() { 
-		return ("Vehicle info" + "\n -----------" + "\nType of ship: " + this.type + "\nhealth of vehicle: " + this.health + "\ndamage of vehicle: " + this.damage
-				+ "\nspeed level: " + this.isSpeed  );
+	public String toString() {
+		return ("Vehicle info" + "\n -----------" + "\nType of ship: " + this.type + "\nhealth of vehicle: "
+				+ this.health + "\ndamage of vehicle: " + this.damage + "\nspeed level: " + this.isSpeed);
 	}
-	
-	public static void healthScore () {
-		
-		
+
+	public static void healthScore() {
+
 	}
-	
-	
-		
-	public static void main(String[] args) {
-		
-		_Ships balancedShip = new _Ships(_ShipType.SHIP_BALANCED, 3, 3, 3 );
-		_Ships tankShip = new _Ships(_ShipType.SHIP_TANK, 5, 2, 1);
-		_Ships speedShip = new _Ships(_ShipType.SHIP_SPEED, 2, 4,5 );
-		
-		System.out.println("this is a test to print out balanced ship stats\n");
-		System.out.println(balancedShip);
-		System.out.println("This is a test to print out tankShip stats");
-		System.out.print(tankShip);
-		System.out.println("This is a test to print out speedShip stats");
-		System.out.println(speedShip);
-		
+
+	public static void main(String[] args) { 
+		 _Ships balancedShip = new _Ships(_ShipType.SHIP_BALANCED, 3, 3, 3 ); _Ships
+		 tankShip = new _Ships(_ShipType.SHIP_TANK, 5, 2, 1); _Ships speedShip = new
+		 _Ships(_ShipType.SHIP_SPEED, 2, 4,5 );
+		 System.out.println("this is a test to print out balanced ship stats\n");
+		 System.out.println(balancedShip);
+		 System.out.println("This is a test to print out tankShip stats");
+		 System.out.println(tankShip);
+		 System.out.println("This is a test to print out speedShip stats");
+		 System.out.println(speedShip);
 	}
 }
