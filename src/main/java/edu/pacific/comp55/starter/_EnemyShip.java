55896dec37cc450ba88cb01f436e59e6248c709b
Divine <d_atah@u.pacific.edu>
2022-11-04 16:33:26 -0700

    Removed redundant functions

--- src/main/java/edu/pacific/comp55/starter/_EnemyShip.java
+++ src/main/java/edu/pacific/comp55/starter/_EnemyShip.java
@@ -10,35 +10,17 @@ public class _EnemyShip extends _Ships {
 		// TODO Auto-generated constructor stub
 	}
 
-	private int enemyShipNum = 0;
 	private Timer movementTimer;
 	private ArrayList<_EnemyShip> shipList = new ArrayList<_EnemyShip>();
 
-	private void incrementShips() {
-		enemyShipNum++;
-	}
-
-	private void decrementShips() {
-		enemyShipNum--;
-	}
-
-
-	public int getShipNum() {
-		return enemyShipNum;
-	}
-
-	public void addEnemyShip(_EnemyShip s) {
-		s = new _EnemyShip(s.type, s.health,s.damage, s.isSpeed);
-		shipList.add(s);
-		incrementShips();
-	}
 	
-	private void removeEnemyShip(_Ships s) {
-		s = new _Ships(s.type, s.health, s.damage, s.isSpeed);
-		decrementShips(); 
-	}
+	/*
+	 * public void addEnemyShip(_EnemyShip s) { s = new _EnemyShip(s.type,
+	 * s.health,s.damage, s.isSpeed); shipList.add(s); } private void
+	 * removeEnemyShip(_Ships s) { s = new _Ships(s.type, s.health, s.damage,
+	 * s.isSpeed); }
+	 */
 	private void bulletsShooting() {}
-
 
 	@Override
 	public String toString() { 
@@ -46,13 +28,12 @@ public class _EnemyShip extends _Ships {
 				+ "\nspeed level: " + this.isSpeed  );
 	}
 	
-
 	public static void main(String[] args) {
 		_EnemyShip grunt1 = new _EnemyShip(_ShipType.GRUNT, 3, 3, 3);
-		grunt1.addEnemyShip(grunt1);
+//		grunt1.addEnemyShip(grunt1);
 		System.out.println("There is 1 grunt with:");
 		System.out.println(grunt1);
-		System.out.println("Number of enemy ships in the array: " + grunt1.getShipNum());
+		System.out.println("Number of enemy ships in the array: ");
 		
 	}
 }
