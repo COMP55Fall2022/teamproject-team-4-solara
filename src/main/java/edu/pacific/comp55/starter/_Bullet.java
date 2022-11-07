3c74e0ba737d2bbc4b66c4bedcb25a1df11b6e92
NathanOsorio <n_osorio2@u.pacific.edu>
2022-11-07 11:11:57 -0800

    added methods into bullet class. As well as instance variables and necessary libraries

--- src/main/java/edu/pacific/comp55/starter/_Bullet.java
+++ src/main/java/edu/pacific/comp55/starter/_Bullet.java
@@ -1,5 +1,33 @@
 package edu.pacific.comp55.starter;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 
+import acm.graphics.*;
 public class _Bullet {
-
+	
+	private ArrayList<GRect> bullets;
+	public static final int WINDOW_WIDTH = 800;
+	public static final int WINDOW_HEIGHT = 600;
+	public static final int BALL_SIZE = 100;
+	public static final int BREAK_MS = 30;
+	public static final int INIT_X_VELOCITY = 5;
+	public static final int NUM_BULLETS = 2; 
+	private GRect bullet;
+	private int xVelocity;
+	private int numTimes; 
+	private _ShipType playerT; 
+	
+	
+	public void actionPerformed(ActionEvent e){
+		
+	}
+	
+	public void moveBullet(){}
+	
+	public void pauseBullet() {}
+	
+	public void resumeBullet() {}
+	
+	public void bulletSound() {}
+	
 }
