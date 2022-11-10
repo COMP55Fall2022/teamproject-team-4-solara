c63e97fea91cea07f8a5925e5ec238fd021389c5
Miguel Constantino-Guzman <m_constantinoguzman@u.pacific.edu>
2022-11-09 17:13:30 -0800

    I made logic for HealthBar.java

--- src/main/java/edu/pacific/comp55/starter/_HealthBar.java
+++ src/main/java/edu/pacific/comp55/starter/_HealthBar.java
@@ -1,5 +1,9 @@
 package edu.pacific.comp55.starter;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 import javax.swing.Timer;
@@ -8,12 +12,52 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class _HealthBar extends GraphicsProgram {
+public class _HealthBar extends GraphicsProgram implements ActionListener {
 	private int healthScore = 2;
-	private GRect healthBar;
+	private GRect healthBarOutline;
+	private GRect healthBar_1, healthBar_2;
+	private Timer timerObj;
+	
+	private final int WINDOW_WIDTH = 1000;
+	private final int WINDOW_HEIGHT = 1000;
+	// Dimensions for Health bar rectangle
+	private final double HEALTHBAR_HEIGHT = 50;
+	private final double HEALTHBAR_WIDTH = 4 * HEALTHBAR_HEIGHT;
+	private final double HEALTHBAR_X = (WINDOW_WIDTH - HEALTHBAR_WIDTH) - (WINDOW_WIDTH * .05);
+	private final double HEALTHBAR_Y = 50;
+	
+	
+	public void init() {
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		requestFocus();
+	}
 	
 	public void run() {
-		healthBar = new GRect(12.0, 12.0);
+		healthBarOutline = new GRect(HEALTHBAR_X, HEALTHBAR_Y, HEALTHBAR_WIDTH, HEALTHBAR_HEIGHT);
+		add(healthBarOutline);
+		
+		timerObj = new Timer(1000, this);
+		
+		healthBar_1 = new GRect(HEALTHBAR_X + 0.5, HEALTHBAR_Y + 0.5, (HEALTHBAR_WIDTH - 0.5) / 2, HEALTHBAR_HEIGHT - 1);
+		healthBar_1.setColor(Color.green);
+		healthBar_1.setFillColor(Color.green);
+		healthBar_1.setFilled(true);
+		add(healthBar_1);
+		
+		healthBar_2 = new GRect(HEALTHBAR_X + (HEALTHBAR_WIDTH / 2), HEALTHBAR_Y + 0.5, (HEALTHBAR_WIDTH - 0.5) / 2, HEALTHBAR_HEIGHT - 1);
+		healthBar_2.setColor(Color.green);
+		healthBar_2.setFillColor(Color.green);
+		healthBar_2.setFilled(true);
+		add(healthBar_2);
+		
+		timerObj.start();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		remove(healthBar_2);
+		timerObj.setDelay(3000);
+		
 	}
 	
 	public boolean damageToShip() {
@@ -37,7 +81,7 @@ public class _HealthBar extends GraphicsProgram {
 	}
 	
 	public static void main(String[] args) {
-		
+		new _HealthBar().start();
 	}
 	
 }
