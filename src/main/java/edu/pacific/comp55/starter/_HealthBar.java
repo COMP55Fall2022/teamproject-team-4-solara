eb1ca859e41beda1077ab510ea60094036eadd8d
Miguel Constantino-Guzman <m_constantinoguzman@u.pacific.edu>
2022-11-09 18:27:30 -0800

    Implemented logic for health bar:
    - added a health bar outline
    - added a for loop that adds N amount of bars based on N amount of
    health that a plan can have
    - added constant final instance variables to help with the logic
    
    Future: I am planning on making a logic to add GRect objects to an ArrayList of GRects so that when a plane gets hit, the GRects disappear or, if the plane gets a health boost, are added back to the health bar...

--- src/main/java/edu/pacific/comp55/starter/_HealthBar.java
+++ src/main/java/edu/pacific/comp55/starter/_HealthBar.java
@@ -15,8 +15,8 @@ import acm.util.RandomGenerator;
 public class _HealthBar extends GraphicsProgram implements ActionListener {
 	private int healthScore = 2;
 	private GRect healthBarOutline;
-	private GRect healthBar_1, healthBar_2;
 	private Timer timerObj;
+	private GLine line;
 	
 	private final int WINDOW_WIDTH = 1000;
 	private final int WINDOW_HEIGHT = 1000;
@@ -25,6 +25,8 @@ public class _HealthBar extends GraphicsProgram implements ActionListener {
 	private final double HEALTHBAR_WIDTH = 4 * HEALTHBAR_HEIGHT;
 	private final double HEALTHBAR_X = (WINDOW_WIDTH - HEALTHBAR_WIDTH) - (WINDOW_WIDTH * .05);
 	private final double HEALTHBAR_Y = 50;
+	
+	private final int HITPOINTS = 8;
 	
 	
 	public void init() {
@@ -36,6 +38,20 @@ public class _HealthBar extends GraphicsProgram implements ActionListener {
 		healthBarOutline = new GRect(HEALTHBAR_X, HEALTHBAR_Y, HEALTHBAR_WIDTH, HEALTHBAR_HEIGHT);
 		add(healthBarOutline);
 		
+		
+		for (int i = 0; i < HITPOINTS; ++i) {
+			GRect healthBar;
+			
+			add(new GLine(HEALTHBAR_X + (i * (HEALTHBAR_WIDTH / HITPOINTS)) , HEALTHBAR_Y, HEALTHBAR_X + (i * (HEALTHBAR_WIDTH / HITPOINTS)), HEALTHBAR_Y + HEALTHBAR_HEIGHT));
+			healthBar = new GRect(HEALTHBAR_X + (i * (HEALTHBAR_WIDTH / HITPOINTS)), HEALTHBAR_Y, HEALTHBAR_WIDTH / HITPOINTS, HEALTHBAR_HEIGHT - 1);
+			healthBar.setFillColor(Color.green);
+			healthBar.setFilled(true);
+			add(healthBar);
+		}
+		
+		
+		
+		/*
 		timerObj = new Timer(1000, this);
 		
 		healthBar_1 = new GRect(HEALTHBAR_X + 0.5, HEALTHBAR_Y + 0.5, (HEALTHBAR_WIDTH - 0.5) / 2, HEALTHBAR_HEIGHT - 1);
@@ -51,13 +67,7 @@ public class _HealthBar extends GraphicsProgram implements ActionListener {
 		add(healthBar_2);
 		
 		timerObj.start();
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		remove(healthBar_2);
-		timerObj.setDelay(3000);
-		
+		*/
 	}
 	
 	public boolean damageToShip() {
