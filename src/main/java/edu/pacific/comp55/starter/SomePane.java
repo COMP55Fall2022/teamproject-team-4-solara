e827bfe8f99644dd4551cbf483f7e30ef8949cc5
NathanOsorio <n_osorio2@u.pacific.edu>
2022-11-16 16:10:30 -0800

    Changinh somePane to switch to our how to play screen

--- src/main/java/edu/pacific/comp55/starter/SomePane.java
+++ src/main/java/edu/pacific/comp55/starter/SomePane.java
@@ -7,34 +7,33 @@ import acm.graphics.GObject;
 public class SomePane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
-	private GImage img;
+	private GImage	howToPlay;
 	private GParagraph para;
 
 	public SomePane(MainApplication app) {
 		this.program = app;
-		img = new GImage("robot head.jpg", 100, 100);
-		para = new GParagraph("welcome\nto my\nsecret room!", 150, 300);
-		para.setFont("Arial-24");
+		howToPlay= new GImage("media/How to play screen.jpg", 0, 0);
+		
 	}
 
 	@Override
 	public void showContents() {
-		program.add(img);
-		program.add(para);
+		program.add(howToPlay);
+		
 	}
 
 	@Override
 	public void hideContents() {
-		program.remove(img);
+		program.remove(howToPlay);
 		program.remove(para);
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		para.setText("you need\nto click\non the eyes\nto go back");
+	/*	para.setText("you need\nto click\non the eyes\nto go back");
 		GObject obj = program.getElementAt(e.getX(), e.getY());
-		if (obj == img) {
+		if (obj == MainMenu) {
 			program.switchToMenu();
-		}
+		} */ // MODIFY THIS TO SWITCH SCREENS WITH BUTTONS INSTEAD 
 	}
 }
