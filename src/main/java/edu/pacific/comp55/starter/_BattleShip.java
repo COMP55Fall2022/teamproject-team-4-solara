1b8ed16ead6c85d071430d3fca7ee32a973ffc0c
Divine <d_atah@u.pacific.edu>
2022-12-05 15:37:20 -0800

    Too easy (prof definitely didn't help with this)
    Expanded on the powerups, tooldesign, levelscreen and battleship classes

--- src/main/java/edu/pacific/comp55/starter/_BattleShip.java
+++ src/main/java/edu/pacific/comp55/starter/_BattleShip.java
@@ -3,25 +3,61 @@ import java.util.*;
 
 import java.util.ArrayList;
 
+import acm.graphics.GImage;
+
 public class _BattleShip extends _Ships {
 	
 	private int health; 
 	private int damage; 
 	private int speed; 
 	private _ShipType type; 
+	private GImage player;
+	private MainApplication screen;
 	
-	public _BattleShip(_ShipType type, int health, int damage, int speed) {
-		super(type, health, damage, speed);
-		this.health = health; 
-		this.damage = damage; 
+	public _BattleShip(_ShipType type, MainApplication app) {
+		super(type, 0, 0, 0);
+		int xBorder = 300;
+		int yBorder = 300;
 		this.type = type; 
-		this.speed = speed; 
-	}
-
-	private Timer t;
-	private int playerNum = 0;
-	private ArrayList<_BattleShip> playerList = new ArrayList<_BattleShip>();
+		this.screen = app;
+		double x = 0;
+		double y = 0;
+		String name = "";
+		switch(this.type) {
+			case SHIP_BALANCED: 
+				this.health = 3;
+				this.damage = 3;
+				this.speed = 3;
+				y = screen.getHeight() - yBorder;
+				x = screen.getWidth() / 3;
+				name = "media/Balanced plane.png";
+				break;
+			case SHIP_TANK: 
+				this.health = 5;
+				this.damage = 2;
+				this.speed = 1;
+				y = screen.getHeight() - yBorder;
+				x = screen.getWidth() / 3 * 2;
+				name = "media/Tank plane.png";
+				break;
+			case SHIP_SPEED:
+				this.health = 1;
+				this.damage = 4;
+				this.speed = 5;
+				y = screen.getHeight() - yBorder;
+				x = screen.getWidth() - xBorder;
+				name = "media/Speed plane.png";
+				break;
+			default:
+		}
+		this.player = new GImage(name,x,y);
+		
+	}	
 	
+	public void showPlayer() {
+		System.out.println("player was added");
+		screen.add(player);
+	}
 	
 	//private _BattleShip b = new _BattleShip(type, health, damage, isSpeed);
 	
@@ -49,24 +85,4 @@ public class _BattleShip extends _Ships {
 		return ("Vehicle info" + "\n -----------" + "\nType of ship: " + this.type + "\nhealth of vehicle: " + this.health + "\ndamage of vehicle: " + this.damage
 				+ "\nspeed level: " + this.isSpeed);
 	}
-
-	private void addPlayers(_BattleShip balanced, _BattleShip tank, _BattleShip speedster) {
-		toString();
-		balanced = new _BattleShip(_ShipType.SHIP_BALANCED, 3, 3, 3); 
-		tank = new _BattleShip(_ShipType.SHIP_TANK, 5, 2, 1);
-		speedster = new _BattleShip(_ShipType.SHIP_SPEED, 1, 4, 5);
-		playerList.add(balanced);
-		playerList.add(tank);
-		playerList.add(speedster);
-	}
-
-	public static void main (String[] args) {
-//		_BattleShip player1 = new _BattleShip(_ShipType.SHIP_BALANCED, 3, 3, 3);
-//		player1.addPlayers(player1);
-//		System.out.println("There is 1 grunt with:");
-//		System.out.println(player1);
-//		System.out.println("Number of enemy ships in the array: " + player1.getPlayerNum());
-
-	}
 }
-
