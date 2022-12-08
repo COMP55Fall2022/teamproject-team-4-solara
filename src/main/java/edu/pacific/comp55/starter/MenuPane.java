c57a9ecf857c3b5739898a4e8e95b17fece3b689
NathanOsorio <n_osorio2@u.pacific.edu>
2022-12-07 16:12:39 -0800

    added color rgb changing to our title "DOGFIGHT"

--- src/main/java/edu/pacific/comp55/starter/MenuPane.java
+++ src/main/java/edu/pacific/comp55/starter/MenuPane.java
@@ -1,13 +1,19 @@
 package edu.pacific.comp55.starter;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+
+import javax.swing.Timer;
+
 import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.graphics.GObject;
 
-public class MenuPane extends GraphicsPane {
+
+public class MenuPane extends GraphicsPane implements ActionListener {
 	private MainApplication program; // you will use program to get access to all of the GraphicsProgram calls
 	private GButton PlayGame;
 	private GButton howToPlay; 
@@ -16,6 +22,15 @@ public class MenuPane extends GraphicsPane {
 	private GLabel title; 
 	private static int SCREEN_WIDTH = 1920;
 	private static int SCREEN_HEIGHT = 1080;
+	 
+	private int redC = 100; 
+	private int greenC = 165; 
+	private int blueC = 232;
+	private boolean redF = true;
+	private boolean blueF = true;
+	private boolean greenF = true;
+	private Color color; 
+	private int i; 
 
 	public MenuPane(MainApplication app) {
 		super();
@@ -25,8 +40,10 @@ public class MenuPane extends GraphicsPane {
 		PlayGame = new GButton("Play Game", (SCREEN_WIDTH / 2) - 300, 355, 210, 50);
 		howToPlay = new GButton("How To Play", (SCREEN_WIDTH / 2) - 300, 420, 210, 50);
 		quit = new GButton ("QUIT", (SCREEN_WIDTH / 2) - 300, 480, 210, 50);
-		title = new GLabel("DOGFIGHT", 650, 150); 
-		title.setFont("Arial-bold-50");
+		
+		
+		
+		 
 		
 		
 		
@@ -38,6 +55,8 @@ public class MenuPane extends GraphicsPane {
 		howToPlay.setFillColor(Color.BLACK);
 		PlayGame.setFillColor(Color.BLACK);
 	}
+	
+	
 
 	@Override
 	public void showContents() {
@@ -45,7 +64,14 @@ public class MenuPane extends GraphicsPane {
 		program.add(PlayGame);
 		program.add(howToPlay);
 		program.add(quit);
+		
+		title = new GLabel("DOGFIGHT", 650, 150); 
+		title.setFont("Arial-bold-50");
 		program.add(title);
+		
+		Timer t = new Timer(25,this);
+		t.start();
+		
 	}
 
 	@Override
@@ -73,4 +99,61 @@ public class MenuPane extends GraphicsPane {
 			System.exit(0);
 		}
 	}
+	@Override 
+	public void actionPerformed (ActionEvent e){ 
+		
+		 i += 50; 
+		 
+		
+		color = new Color(redC, blueC, greenC); 
+		title.setColor(color);
+		
+		
+		if (redC == 0 ) {
+			redF = true; 
+		}
+		
+		else if (redC == 255) {
+			redF = false; 
+		}
+		
+		if (blueC == 50) {
+			blueF = true; 
+		}
+		
+		else if (blueC == 232) {
+			blueF = false;
+		}
+		
+		if (greenC == 20) {
+			greenF = true;
+		}
+		
+		else if (greenC == 165) {
+			greenF = false; 
+		}
+		
+		if(redF) {
+			++redC;
+		}
+		else {
+			--redC; 
+		}
+		
+		if(blueF) {
+			++blueC;
+		}
+		else {
+			--blueC;
+		}
+		
+		if(greenF) {
+			++greenC;
+		}
+		else {
+			--greenC;
+		}
+	}
+	
+	
 }
